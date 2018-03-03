1b9c6b84a64e73a0cf39ea28d72893dd364c643b
khanhduyle <khanhduyle@assembla.com>
2018-03-03 01:41:16 -0800

    add

---
+++ src/Leetcode/lengthLongestSubstring.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] c = new int[256];
+        Arrays.fill(c,0);
+        int j = 0;
+        int i = 0;
+        int max = 0;
+        while (j < s.length()) {
+            if (i < s.length() && c[s.charAt(i)] == 0) {
+                c[s.charAt(i)] = 1;
+                    i++;
+            } 
+            if (i == s.length() || c[s.charAt(i)] ==  1) {
+                max = Math.max(max, i - j);
+                c[s.charAt(j)] = 0;
+                j++;
+            }
+        }
+        return max;       
+    }
+    public static void main(String[] args) {
+        String s = "sfsdcdw";
+        int len = lengthOfLongestSubstring(s);
+        System.out.println(len);
+    }
+}
